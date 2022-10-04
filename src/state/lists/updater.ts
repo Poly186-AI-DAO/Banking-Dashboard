cbd7eae62bcae6c4538d5e92d09cafcd6f040973
Khalifa Yakub Mohammed <khalifa_yakub@outlook.com>
2022-10-04 13:37:35 +0100

    Added P8 to token list

--- src/state/lists/updater.ts
+++ src/state/lists/updater.ts
@@ -12,6 +12,18 @@ export default function Updater() {
     if (chainId)
       Fetcher.fetchDxDaoTokenList(chainId)
         .then((tokenList: TokenList) => {
+          // Fetcher.fetchTokenData(chainId, '0x772fbC4a49998003c6291dB0eA44FE498EeF2bb3', library).then(tokenData => {
+          //   console.log(tokenData)
+          // })
+          tokenList.tokens.push({
+            chainId: 100,
+            address: '0x772fbC4a49998003c6291dB0eA44FE498EeF2bb3',
+            name: 'P8',
+            symbol: 'P8',
+            decimals: 18,
+            logoURI: 'https://tokens.1inch.exchange/0x6b175474e89094c44da98b954eedeac495271d0f.png'
+          })
+          console.log(tokenList.tokens)
           if (tokenList) dispatch(setTokenList(tokenList))
         })
         .catch(error => {
